081c2fe45c6b11734321eed86c6c74882804b4e7
Chris Conley <chrisc93@vt.edu>
2016-09-26 10:03:51 -0400

    Add Kernel Adiutor dashboard tile credit to @tsubus for his setting dashboard implementation
    Signed-off-by: Chris Conley <chrisc93@vt.edu>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -161,5 +161,6 @@ public class Settings extends SettingsActivity {
     public static class SubstratumActivity extends SettingsActivity { /* empty */ }
     public static class SuperSUActivity extends SettingsActivity { /* empty */ }
     public static class SuperUserActivity extends SettingsActivity { /* empty */ }
+    public static class KernelAdiutorActivity extends SettingsActivity { /* empty */ }
     public static class AmbientDisplaySettingsActivity extends SettingsActivity { /* empty */ }
 }
